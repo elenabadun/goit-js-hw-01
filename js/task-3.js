c66ed41a71448d578ce92dfee5af83da3cda0c22
Elena <elena.badyn@icloud.com>
2025-10-02 10:33:07 +0200

    solved mistake

--- js/task-3.js
+++ js/task-3.js
@@ -12,7 +12,7 @@ function getElementWidth(content, padding, border) {
   const totalWidth =
     Number.parseFloat(content) +
     (Number.parseFloat(padding) + Number.parseFloat(border)) * 2;
-  return totalWidth + "px";
+  return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
